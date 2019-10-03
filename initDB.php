219bac93c0f7b8a880a3bdd5bc2ac1868e606019
jankikhatri <jankikhatri72@gmail.com>
2019-10-03 19:18:40 -0400

    update

--- initDB.php
+++ initDB.php
@@ -13,7 +13,32 @@ $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
 //	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 	$db = new PDO($conn_string, $username, $password);
-	echo "Connected";
+	echo " Connected";
+
+	//create table
+	$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	$stmt = $db->prepare($query);
+	print_r($stmt->errorInfo());
+	$r = $stmt->execute();
+	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
+	unset($r);
+	//simple insert
+	//TODO/homework make values variables bindable
+	$insert_query = "INSERT INTO `TestUsers`( `username`, `pin`) VALUES ('JohnDoe', 1234)";
+	$stmt = $db->prepare($insert_query);
+	$r = $stmt->execute();
+	//TODO catch error from DB
+	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
+	
+	//TODO select query using bindable :username is where clause
+	//select * from TestUsers where username = 
+
 }
 catch(Exception $e){
 	echo $e->getMessage();
