dbb223036605d8ffb60ccd49de9b82de11230f25
jankikhatri <jankikhatri72@gmail.com>
2019-10-08 18:53:21 -0400

    updated

--- initDB.php
+++ initDB.php
@@ -4,14 +4,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 //load the config from the same directory
 require('config.php');
 echo "Loaded host: " . $host;
 
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
-//	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 	$db = new PDO($conn_string, $username, $password);
 	echo " Connected";
 
@@ -27,17 +25,26 @@ try{
 	print_r($stmt->errorInfo());
 	$r = $stmt->execute();
 	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
-	unset($r);
-	//simple insert
-	//TODO/homework make values variables bindable
-	$insert_query = "INSERT INTO `TestUsers`( `username`, `pin`) VALUES ('JohnDoe', 1234)";
+ 
+	//insert
+  $user = "JohnDoe";
+  $pin = 1234;
+	$insert_query = "INSERT INTO `TestUsers`( `username`, `pin`) VALUES (:username, :pin)";
 	$stmt = $db->prepare($insert_query);
-	$r = $stmt->execute();
+	$r = $stmt->execute(array(":username"=> $user, ":pin"=> $pin));
+  print_r($stmt->errorInfo());
+  
 	//TODO catch error from DB
 	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
 	
-	//TODO select query using bindable :username is where clause
 	//select * from TestUsers where username = 
+ /*$select_query = "select * from 'TestUsers' where username = :username";
+ $stmt = $db->prepare($select_query);
+ $r = $stmt->execute(array(":username"=> $user)); 
+ 
+ //previous connection/query prep/etc
+ $results = $stmt->fetch(PDO::FETCH_ASSOC);
+ echo "<pre>" . var_export($results, true) . "</pre>"; */
 
 }
 catch(Exception $e){
