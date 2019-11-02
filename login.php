95d89a38b19a5a78eda1362bc949a4f392d15a59
jankikhatri <jankikhatri72@gmail.com>
2019-11-02 02:45:55 -0400

    project

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require("config.php");
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+  }
+
+try
+{
+  $db = new PDO($conn_string, $username, $password);
+  $query = 'SELECT * FROM Register WHERE USERNAME = :username';
+
+  $stmt = $db->prepare($query);
+ 
+  $stmt->bindParam(':username', $user);
+  $r = $stmt->execute();
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  
+  if($_POST['password'] == $result['password']) {
+        	echo "<html>Login Successful! <br></html>";
+          echo "Welcome!";
+    }
+	else {
+        	echo "Invalid username/password. Please try <a href='index.html'>again</a>.";
+    }
+  
+  
+}
+
+catch(Exception $e)
+{
+  echo $e->getMessage();
+  echo "Something went wrong";
+}
+?>
